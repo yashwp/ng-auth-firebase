c2a851cb8f0cda86c0b34c15ae71e57806ba7c6e
Yashwardhan Pauranik <yash@appointy.com>
2018-03-14 19:55:04 +0530

    angular fire 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {environment} from '../environments/environment';
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
-
+// Components
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase, 'ng-auth-firebase'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAml6u7RzEk1X9YnqhWdrfdn9VBhfExX4c',
+    authDomain: 'ng-auth-firebase.firebaseapp.com',
+    databaseURL: 'https://ng-auth-firebase.firebaseio.com',
+    projectId: 'ng-auth-firebase',
+    storageBucket: 'ng-auth-firebase.appspot.com',
+    messagingSenderId: '98872694644'
+  }
 };
