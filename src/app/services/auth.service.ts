07f3ebd262f89fa600db3eb0fd66c603c5895645
Yashwardhan Pauranik <yash@appointy.com>
2018-03-14 20:13:38 +0530

    firebase.authstate

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  private user: Observable<firebase.User>;
+
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = this.firebaseAuth.authState;
+  }
 
 }
