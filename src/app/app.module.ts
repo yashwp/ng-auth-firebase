192c217f9a1a9af3e2db605d9209b3e33fdffd55
Yashwardhan Pauranik <yash@appointy.com>
2018-03-18 00:12:34 +0530

    firestore started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import {environment} from '../environments/environment';
-import {AngularFireModule} from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
@@ -28,7 +30,9 @@ import {FormsModule} from '@angular/forms';
     MDBBootstrapModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase, 'ng-auth-firebase'),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../services/auth.service';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +9,26 @@ import {AuthService} from '../services/auth.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  employees: Observable<any[]>;
+  employee = {
+    name: '',
+    age: 0,
+    isActive: true,
+    dob: new Date()
+  };
 
-  ngOnInit() {
+  constructor(private authService: AuthService,
+              private db: AngularFirestore) {
+    this.employees = db.collection('/employees').valueChanges();
   }
 
+  ngOnInit() {
+    // this.addEmployee();
+  }
+
+  addEmployee() {
+    this.db.collection('/employees').add(this.employee);
+  }
   signout() {
     this.authService.logout();
     localStorage.clear();
